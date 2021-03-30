328219d3979a72d5e0321a95528243aa97428529
celia22 <celialopez22@gmail.com>
2021-03-30 17:52:24 +0200

    finish iteration5, one error on iteration 2, one error on iteration 3

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
 
-  max() {}
+  // deleteBrains() {
+  //   generatedBrain.forEach((item, index) => {
+  //     if (item.y > canvas.height) {
+  //       generatedBrain.splice(item, index);
+  //     }
+  //   });
+  // }
 
-  min() {}
 
-  sum() {}
+  get(pos) {
+    this.items.forEach((item, pos) => {
+      if (!pos) {
+        throw new Error('OutOfBounds');
+      }
+      return item(pos)
+    })
 
-  avg() {}
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let max = Math.max(...this.items);
+    return max
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let min = Math.min(...this.items);
+    return min
+  }
+
+  sum() { }
+
+  avg() { }
 }
 
 module.exports = SortedList;
